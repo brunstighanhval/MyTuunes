f81594995307e3da1c74deb25f9bad2bb08798a3
brunstighanhval <mlkaer1@hotmail.com>
2022-11-28 09:15:26 +0100

    Main
    Main

--- src/gui/Main.java
+++ src/gui/Main.java
@@ -1,7 +1,24 @@
 package gui;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import static javafx.application.Application.launch;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("View/MovieView.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
     }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
 }
